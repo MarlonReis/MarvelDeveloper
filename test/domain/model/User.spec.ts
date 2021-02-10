5041c50e42c257c2dd88b6c79154da5816e63890
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-10 16:53:24 -0300

    test: create user entity

---
+++ test/domain/model/User.spec.ts
@@ -0,0 +1,64 @@
+import { StatusUser, User } from '@/domain/model/user/User'
+
+describe('User', () => {
+  test('should create user instance with success', () => {
+    const response = User.create({
+      name: 'Valid Name',
+      email: 'valid@example.com',
+      password: 'Valid@Password'
+    })
+
+    expect(response.value as User).toMatchObject({
+      name: { value: 'Valid Name' },
+      email: { value: 'valid@example.com' },
+      password: { value: 'Valid@Password' },
+      comicsReactions: [],
+      charactersReactions: [],
+      profileImage: undefined,
+      status: StatusUser.CREATED,
+      createAt: expect.any(Date)
+    })
+  })
+
+  test('should return invalid param error when name is invalid', () => {
+    const response = User.create({
+      name: 'er',
+      email: 'valid@example.com',
+      password: 'Valid@Password'
+    })
+
+    expect(response.isFailure()).toBe(true)
+    expect(response.isSuccess()).toBe(false)
+    expect(response.value).toMatchObject({
+      message: "Attribute 'name' equals 'er' is invalid!"
+    })
+  })
+
+  test('should return invalid param error when email is invalid', () => {
+    const response = User.create({
+      name: 'Valid Name',
+      email: 'valid-example.com',
+      password: 'Valid@Password'
+    })
+
+    expect(response.isFailure()).toBe(true)
+    expect(response.isSuccess()).toBe(false)
+    expect(response.value).toMatchObject({
+      message: "Attribute 'email' equals 'valid-example.com' is invalid!"
+    })
+  })
+
+  test('should return invalid param error when password is invalid', () => {
+    const response = User.create({
+      name: 'Valid Name',
+      email: 'valid@example.com',
+      password: 'Err'
+    })
+
+    expect(response.isFailure()).toBe(true)
+    expect(response.isSuccess()).toBe(false)
+    expect(response.value).toMatchObject({
+      message: "Attribute 'password' equals 'Err' is invalid!"
+    })
+  })
+})
