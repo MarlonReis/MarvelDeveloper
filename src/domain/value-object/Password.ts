81f04be8208d07f9a85d8832a442871c8c1f724f
Marlon Gonçalves Reis <marlon.reis@smartmeil.com.br>
2021-02-10 11:23:09 -0300

    test: create password value

--- src/domain/value-object/Password.ts
+++ src/domain/value-object/Password.ts
@@ -4,19 +4,19 @@ import { InvalidParamError } from '@/domain/errors'
 export class Password {
     public readonly value: string
 
-    private constructor(password: string) {
+    private constructor (password: string) {
         this.value = password
         Object.freeze(this)
     }
 
-    static create(password: string): Either<InvalidParamError, Password> {
+    static create (password: string): Either<InvalidParamError, Password> {
         if (password && /.{8,}/.test(password)) {
             return success(new Password(password))
         }
         return failure(new InvalidParamError('password', password))
     }
 
-    getValue(): string {
+    getValue (): string {
         return this.value
     }
 }
---
+++ test/domain/value-object/Password.spec.ts
@@ -0,0 +1,46 @@
+import { InvalidParamError } from '@/domain/errors'
+import { Password } from '@/domain/value-object/Password'
+
+describe('Password', () => {
+    test('should create valid password', () => {
+        const result = Password.create('Valid-Password')
+        expect(result.isSuccess()).toBe(true)
+        expect(result.value).toBeInstanceOf(Password)
+        expect((result.value as any).getValue()).toBe('Valid-Password')
+    })
+
+    test('should return failure equals true when password is invalid', () => {
+        const result = Password.create('invalid')
+        expect(result.isFailure()).toBe(true)
+        expect(result.value).toBeInstanceOf(InvalidParamError)
+    })
+
+    test('should return failure equals true when password is undefined', () => {
+        const result = Password.create(undefined)
+        expect(result.isFailure()).toBe(true)
+        expect(result.value).toBeInstanceOf(InvalidParamError)
+    })
+
+    test('should return password value', () => {
+        const result = Password.create('Valid@Password')
+        const password = result.value
+        expect(password).toMatchObject({
+            value: 'Valid@Password'
+        })
+    })
+
+    test('should return message error', () => {
+        const result = Password.create('Valid')
+        const password = result.value
+        expect(password).toMatchObject({
+            message: "Attribute 'password' equals 'Valid' is invalid!"
+        })
+    })
+
+
+    test('should return failure is true when password is less then 8 characters', () => {
+        const result = Password.create('error')
+        expect(result.isFailure()).toBe(true)
+        expect(result.value).toBeInstanceOf(InvalidParamError)
+    })
+})
