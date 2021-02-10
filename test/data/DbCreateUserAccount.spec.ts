d02907df1b20f6e5a61b60ca46fdfca6a4004618
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-10 18:29:29 -0300

    refactor: move class creation to factory method

--- test/data/DbCreateUserAccount.spec.ts
+++ test/data/DbCreateUserAccount.spec.ts
@@ -6,30 +6,51 @@ import { EncryptsPassword } from './protocol/EncryptsPassword'
 import { CreateUserAccountRepository } from './repository/CreateUserAccountRepository'
 import { FindUserAccountByEmailRepository } from './repository/FindUserAccountByEmailRepository'
 
+const createUserAccountRepositoryStubFactory = (): CreateUserAccountRepository => {
+  class CreateUserAccountRepositoryStub implements CreateUserAccountRepository {
+    async execute (data: CreateUserData): Promise<Either<RepositoryInternalError, void>> {
+      return await Promise.resolve(success())
+    }
+  }
+  return new CreateUserAccountRepositoryStub()
+}
+
+const findUserAccountByEmailRepositoryStubFactory = (): FindUserAccountByEmailRepository => {
+  class FindUserAccountByEmailRepositoryStub implements FindUserAccountByEmailRepository {
+    async execute (email: string): Promise<Either<NotFoundError, any>> {
+      return await Promise.resolve(failure(new Error('Any error')))
+    }
+  }
+  return new FindUserAccountByEmailRepositoryStub()
+}
+
+const encryptsPasswordStubFactory = (): EncryptsPassword => {
+  class EncryptsPasswordStub implements EncryptsPassword {
+    async execute (data: string): Promise<string> {
+      return await Promise.resolve('ValidDataEncrypt')
+    }
+  }
+  return new EncryptsPasswordStub()
+}
+
+interface TypeSut {
+  encryptsPasswordStub: EncryptsPassword
+  repositoryStub: CreateUserAccountRepository
+  findUserByEmailStub: FindUserAccountByEmailRepository
+  sut: DbCreateUserAccount
+}
+
+const makeSutFactory = (): TypeSut => {
+  const encryptsPasswordStub = encryptsPasswordStubFactory()
+  const repositoryStub = createUserAccountRepositoryStubFactory()
+  const findUserByEmailStub = findUserAccountByEmailRepositoryStubFactory()
+  const sut = new DbCreateUserAccount(repositoryStub, findUserByEmailStub, encryptsPasswordStub)
+  return { repositoryStub, encryptsPasswordStub, findUserByEmailStub, sut }
+}
+
 describe('DbCreateUserAccount', () => {
   test('should create user account', async () => {
-    class CreateUserAccountRepositoryStub implements CreateUserAccountRepository {
-      async execute (data: CreateUserData): Promise<Either<RepositoryInternalError, void>> {
-        return await Promise.resolve(success())
-      }
-    }
-
-    class FindUserAccountByEmailRepositoryStub implements FindUserAccountByEmailRepository {
-      async execute (email: string): Promise<Either<NotFoundError, any>> {
-        return await Promise.resolve(failure(new Error('Any error')))
-      }
-    }
-
-    class EncryptsPasswordStub implements EncryptsPassword {
-      async execute (data: string): Promise<string> {
-        return await Promise.resolve('ValidDataEncrypt')
-      }
-    }
-
-    const encryptsPasswordStub = new EncryptsPasswordStub()
-    const repositoryStub = new CreateUserAccountRepositoryStub()
-    const findUserByEmailStub = new FindUserAccountByEmailRepositoryStub()
-    const sut = new DbCreateUserAccount(repositoryStub, findUserByEmailStub, encryptsPasswordStub)
+    const { sut } = makeSutFactory()
 
     const response = await sut.execute({
       name: 'Valid Name',
