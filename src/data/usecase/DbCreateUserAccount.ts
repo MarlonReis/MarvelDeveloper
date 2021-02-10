496d65b544565dab7889902783f7cb213c9eeaba
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-10 18:18:22 -0300

    feat: implement create user account use case

---
+++ src/data/usecase/DbCreateUserAccount.ts
@@ -0,0 +1,36 @@
+import { InvalidParamError } from '@/domain/errors'
+import { CreateUserData } from '@/domain/model/user/UserData'
+import { CreateUserAccount } from '@/domain/usecase/CreateUserAccount'
+import { Either, failure, success } from '@/shared/Either'
+import { CreateUserAccountRepository } from '@/data/repository/CreateUserAccountRepository'
+import { FindUserAccountByEmailRepository } from '@/data/repository/FindUserAccountByEmailRepository'
+import { EncryptsPassword } from '../protocol/EncryptsPassword'
+
+export class DbCreateUserAccount implements CreateUserAccount {
+  private readonly createUserAccount: CreateUserAccountRepository
+  private readonly findUserAccountByEmail: FindUserAccountByEmailRepository
+  private readonly encryptsPassword: EncryptsPassword
+
+  constructor (
+    createUserAccountRepository: CreateUserAccountRepository,
+    findUserAccountByEmail: FindUserAccountByEmailRepository,
+    encryptsPassword: EncryptsPassword
+  ) {
+    this.createUserAccount = createUserAccountRepository
+    this.findUserAccountByEmail = findUserAccountByEmail
+    this.encryptsPassword = encryptsPassword
+  }
+
+  async execute (data: CreateUserData): Promise<Either<InvalidParamError, void>> {
+    const userFound = await this.findUserAccountByEmail.execute(data.email)
+
+    if (userFound.isFailure()) {
+      const password = await this.encryptsPassword.execute(data.password)
+      const response = await this.createUserAccount.execute({ ...data, password })
+      if (response.isSuccess()) {
+        return await Promise.resolve(success())
+      }
+      return await Promise.resolve(failure<any, any>(response.value))
+    }
+  }
+}
