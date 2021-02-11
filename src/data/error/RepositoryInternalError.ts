5f793e29658421f57d1a6f5691243dc5fbb8ce51
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-10 22:28:39 -0300

    refactor: add controtor with cuse error

--- src/data/error/RepositoryInternalError.ts
+++ src/data/error/RepositoryInternalError.ts
@@ -1,8 +1,8 @@
 export class RepositoryInternalError extends Error {
-  // public readonly cause: Error
-  // constructor(cause: Error) {
-  //   super(cause.message)
-  //   this.cause = cause
-  //   Object.freeze(this)
-  // }
+  public readonly cause: Error
+  constructor (cause: Error) {
+    super(cause.message)
+    this.cause = cause
+    Object.freeze(this)
+  }
 }
