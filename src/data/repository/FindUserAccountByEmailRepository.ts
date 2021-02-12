4da34c6e8eb0ea44d4f6d3520bd70d5f4a83544b
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-12 17:49:51 -0300

    refactor: add user account response

--- src/data/repository/FindUserAccountByEmailRepository.ts
+++ src/data/repository/FindUserAccountByEmailRepository.ts
@@ -1,6 +1,7 @@
 import { Either } from '@/shared/Either'
 import { NotFoundError } from '@/data/error'
+import { UserAccountResponse } from '@/domain/model/user/UserData'
 
 export interface FindUserAccountByEmailRepository {
-  execute: (email: string) => Promise<Either<NotFoundError, any>>
+  execute: (email: string) => Promise<Either<NotFoundError, UserAccountResponse>>
 }
