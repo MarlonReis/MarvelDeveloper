c2880f4a6d572edf1caa85e63c6f6b285bfcf443
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-12 12:28:44 -0300

    refactor: create create user account orm repository

---
+++ src/infrastructure/database/orm/repository/CreateUserAccountORMRepository.ts
@@ -0,0 +1,27 @@
+import { CreateUserAccountRepository } from '@/data/repository/CreateUserAccountRepository'
+import { ConnectionDatabase } from '@/infrastructure/database/protocol/ConnectionDatabase'
+import { UserOrm } from '@/infrastructure/database/orm/model/UserOrm'
+import { CreateUserData } from '@/domain/model/user/UserData'
+import { Either, failure, success } from '@/shared/Either'
+import { RepositoryInternalError } from '@/data/error'
+
+import { Connection } from 'typeorm'
+
+export class CreateUserAccountORMRepository implements CreateUserAccountRepository {
+  private readonly connectionDatabase: ConnectionDatabase<Connection>
+
+  constructor (connection: ConnectionDatabase<Connection>) {
+    this.connectionDatabase = connection
+  }
+
+  async execute (data: CreateUserData): Promise<Either<RepositoryInternalError, void>> {
+    try {
+      const user = UserOrm.create(data)
+      const repository = this.connectionDatabase.connection().getRepository(UserOrm)
+      await repository.save(user.value)
+      return success()
+    } catch (error) {
+      return failure(new RepositoryInternalError(error))
+    }
+  }
+}
