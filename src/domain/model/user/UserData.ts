5f065ba1d9ee0f30a64634731dff68cfaa148021
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-12 17:46:46 -0300

    feat: create user account response

--- src/domain/model/user/UserData.ts
+++ src/domain/model/user/UserData.ts
@@ -1,5 +1,13 @@
+import { StatusUser } from './StatusUser'
+
 export interface CreateUserData {
   name: string
   email: string
   password: string
+}
+
+export interface UserAccountResponse {
+  name: string
+  email: string
+  status: StatusUser
 }
