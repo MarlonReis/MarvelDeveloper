caccbc1550dcc43afd36972ead24a948269bdecc
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-13 13:03:15 -0300

    feat: return status http 422 when feald is invalid

--- src/presentation/controller/create-user-account/CreateUserAccountController.ts
+++ src/presentation/controller/create-user-account/CreateUserAccountController.ts
@@ -1,5 +1,29 @@
 import { CreateUserAccount } from '@/domain/usecase/CreateUserAccount'
+import { MissingParamError } from '@/presentation/error'
+import { unProcessableEntity } from '@/presentation/helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const fieldInvalid = {
+    name: (value: string): boolean => {
+        if (value && /.{3,}/.test(value)) {
+            return false
+        }
+        return true
+    },
+    email: (value: string): boolean => {
+        if (value && emailRegex.test(value)) {
+            return false
+        }
+        return true
+    },
+    password: (value: string): boolean => {
+        if (value && /.{8,}/.test(value)) {
+            return false
+        }
+        return true
+    }
+}
 
 export class CreateUserAccountController implements Controller {
   private readonly createUserAccount: CreateUserAccount
@@ -9,6 +33,15 @@ export class CreateUserAccountController implements Controller {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['name', 'email', 'password']
+
+    for (const field of requiredFields) {
+      const value = request.body[field]
+      if (fieldInvalid[field](value)) {
+        return unProcessableEntity(new MissingParamError(field))
+      }
+    }
+
     const response = await this.createUserAccount.execute(request.body)
     if (response.isSuccess()) {
       return await Promise.resolve({
