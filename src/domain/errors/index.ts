feccf4a6c36e3bf207132824b20043eb9b11208b
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-13 14:19:28 -0300

    refactor: remove error of domain to presentation

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,2 +1,1 @@
 export * from './InvalidParamError'
-export * from './InternalServerError'
--- src/presentation/error/InternalServerError.ts
+++ src/presentation/error/InternalServerError.ts
@@ -1,6 +1,5 @@
 export class InternalServerError extends Error {
   constructor (public readonly cause: Error) {
     super('Internal server error')
-    this.name = 'InternalServerError'
   }
 }
