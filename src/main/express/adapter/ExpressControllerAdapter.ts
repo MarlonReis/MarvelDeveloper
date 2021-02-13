9f1c42cdb81b2986fac705f45b730c3971a3df17
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-13 15:17:12 -0300

    feat: create express adapter

---
+++ src/main/express/adapter/ExpressControllerAdapter.ts
@@ -0,0 +1,23 @@
+
+import { Request, Response } from 'express'
+
+import { Controller } from '@/presentation/protocols/Controller'
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http'
+
+export const expressAdapter = (controller: Controller) => {
+  return async (req: Request, res: Response): Promise<any> => {
+    const { body, params } = req
+
+    const request: HttpRequest = { body, params }
+    const response: HttpResponse = await controller.handle(request)
+
+    if (response.statusCode === 200 || response.statusCode === 201) {
+      res.status(response.statusCode).json(response.body)
+    } else {
+      res.status(response.statusCode).json({
+        error: response.body.name,
+        message: response.body.message
+      })
+    }
+  }
+}
