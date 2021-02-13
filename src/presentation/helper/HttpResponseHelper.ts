f08ca20c0fd9887e4aef26ad12aa92450ca74f0f
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-13 13:37:33 -0300

    feat: add more http response

--- src/presentation/helper/HttpResponseHelper.ts
+++ src/presentation/helper/HttpResponseHelper.ts
@@ -1,11 +1,21 @@
+import { InternalServerError } from '@/domain/errors'
 import { HttpResponse } from '@/presentation/protocols/Http'
+
+export const createSuccess = (): HttpResponse => ({
+    statusCode: 201
+})
+
+// export const ok = (body?: any): HttpResponse => ({
+//     statusCode: 200,
+//     body
+// })
 
 export const unProcessableEntity = (error: Error): HttpResponse => ({
     statusCode: 422,
     body: error
 })
 
-// export const internalServerError = (causeError: Error): HttpResponse => ({
-//     statusCode: 500,
-//     body: new InternalServerError(causeError)
-// })
+export const internalServerError = (causeError: Error): HttpResponse => ({
+    statusCode: 500,
+    body: new InternalServerError(causeError)
+})
