c75ce5b9e46ad640c28caf82d6ec77e8543f7f5d
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-13 23:22:38 -0300

    test: call logger system integration

--- test/main/factories/CreateUserAccountFactory.test.ts
+++ test/main/factories/CreateUserAccountFactory.test.ts
@@ -1,10 +1,16 @@
 import { DbCreateUserAccount } from '@/data/usecase/DbCreateUserAccount'
 import { CreateUserAccountFactory } from '@/main/factories/CreateUserAccountFactory'
+import { LogControllerDecorator } from '@/main/factories/LogControllerDecorator'
+const sut = new CreateUserAccountFactory()
 
 describe('CreateUserAccountFactory', () => {
-  test('should create a factory', () => {
-    const sut = new CreateUserAccountFactory()
+  test('should create DbCreateUserAccountFactory', () => {
     const response = sut.makeCreateUserAccountFactory()
     expect(response).toBeInstanceOf(DbCreateUserAccount)
+  })
+
+  test('should create Controller factory', () => {
+    const response = sut.makeControllerFactory()
+    expect(response).toBeInstanceOf(LogControllerDecorator)
   })
 })
