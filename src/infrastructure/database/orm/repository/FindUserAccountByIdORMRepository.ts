536aff540102b4a967a2895f3c7054b2a9586f57
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-14 20:24:53 -0300

    feat: return failure when repository throws error

--- src/infrastructure/database/orm/repository/FindUserAccountByIdORMRepository.ts
+++ src/infrastructure/database/orm/repository/FindUserAccountByIdORMRepository.ts
@@ -1,4 +1,4 @@
-import { NotFoundError } from '@/data/error'
+import { NotFoundError, RepositoryInternalError } from '@/data/error'
 import { FindUserAccountByIdRepository } from '@/data/repository/FindUserAccountByIdRepository'
 import { UserAccountResponse } from '@/domain/model/user/UserData'
 import { Either, failure, success } from '@/shared/Either'
@@ -6,7 +6,7 @@ import { Either, failure, success } from '@/shared/Either'
 import { ConnectionDatabase } from '@/infrastructure/database/protocol/ConnectionDatabase'
 
 import { Connection } from 'typeorm'
-import { UserOrm } from '../model/UserOrm'
+import { UserOrm } from '@/infrastructure/database/orm/model/UserOrm'
 
 export class FindUserAccountByIdORMRepository implements FindUserAccountByIdRepository {
   private readonly connectionDatabase: ConnectionDatabase<Connection>
@@ -15,22 +15,25 @@ export class FindUserAccountByIdORMRepository implements FindUserAccountByIdRepo
     this.connectionDatabase = connection
   }
 
-  async execute (id: string): Promise<Either<NotFoundError, UserAccountResponse>> {
-    const user = await this.connectionDatabase
-      .connection().getRepository(UserOrm)
-      .createQueryBuilder('users')
-      .where('users.id = :id', { id })
-      .getOne()
+  async execute (id: string): Promise<Either<NotFoundError | RepositoryInternalError, UserAccountResponse>> {
+    try {
+      const user = await this.connectionDatabase.connection().getRepository(UserOrm)
+        .createQueryBuilder('users')
+        .where('users.id = :id', { id })
+        .getOne()
 
-    if (user) {
-      return success({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        status: user.status,
-        profileImage: user.profileImage
-      })
+      if (user) {
+        return success({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          status: user.status,
+          profileImage: user.profileImage
+        })
+      }
+      return failure(new NotFoundError(`Cannot found account by id equals '${id}'!`))
+    } catch (error) {
+      return failure(new RepositoryInternalError(error))
     }
-    return failure(new NotFoundError(`Cannot found account by id equals '${id}'!`))
   }
 }
