26a8d0a537e9aa5c044e832d9d25e37337731eb5
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-15 15:37:00 -0300

    test: router response error

--- test/main/express/routers/UpdateUserAccountRouter.test.ts
+++ test/main/express/routers/UpdateUserAccountRouter.test.ts
@@ -15,6 +15,16 @@ const defaultUserData = {
   profileImage: 'path-image'
 }
 
+const requestPut = request(app).put('/api/account')
+
+const insertUser = async (connectionDatabase: any, userData: any): Promise<string> => {
+  const result = await connectionDatabase.connection()
+    .createQueryBuilder().insert().into(UserOrm)
+    .values(userData).execute()
+  const { id } = result.identifiers[0]
+  return id
+}
+
 describe('UpdateUserAccountRouter', () => {
 
   beforeAll(async () => {
@@ -31,15 +41,63 @@ describe('UpdateUserAccountRouter', () => {
     await connectionDatabase.close()
   })
 
-  test('should return statusCode 200 when do update with success', async () => {
-    const result = await connectionDatabase.connection()
-      .createQueryBuilder().insert().into(UserOrm)
-      .values(defaultUserData).execute()
-    const { id } = result.identifiers[0]
+  test('should return statusCode 422 when id is undefined', async () => {
+    await requestPut.send(defaultUserData).expect(422, {
+      error: 'MissingParamError',
+      message: "Attribute 'id' is invalid!"
+    })
+  })
 
-    await request(app).put('/api/account').send({
+  test('should return statusCode 422 when email is invalid', async () => {
+    await requestPut.send({
+      ...defaultUserData,
+      id: 'valid-id',
+      email: 'invalid-email'
+    }).expect(422, {
+      error: 'InvalidParamError',
+      message: "Attribute 'email' equals 'invalid-email' is invalid!"
+    })
+  })
+
+
+  test('should return statusCode 200 when do update with success', async () => {
+    const id = await insertUser(connectionDatabase, defaultUserData)
+
+    await requestPut.send({
       ...defaultUserData, id
     }).expect(200)
   })
+
+  test('should return statusCode 404 when repository throw error', async () => {
+    await requestPut.send({
+      ...defaultUserData, id: 'id-not-exist'
+    }).expect(404, {
+      error: 'NotFoundError',
+      message: "Cannot found account by id equals 'id-not-exist'!"
+    })
+  })
+
+
+  test('should return statusCode 400 when try to use an email that is already in use', async () => {
+    await insertUser(connectionDatabase, {
+      ...defaultUserData,
+      id: undefined,
+      email: "any-other@in-use.com"
+    })
+
+    const id = await insertUser(connectionDatabase, {
+      ...defaultUserData, id: undefined
+    })
+
+    await requestPut.send({
+      ...defaultUserData,
+      email: 'any-other@in-use.com',
+      id
+    }).expect(400, {
+      error: 'BadRequestError',
+      message: "Email 'any-other@in-use.com' is already being used by another account!"
+    })
+  })
+
 
 })
