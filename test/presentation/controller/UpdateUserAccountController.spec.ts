7a0fed023d09195a0c76dc0f763c41eb2f0d2111
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-15 13:01:09 -0300

    test: return correct errors

--- test/presentation/controller/UpdateUserAccountController.spec.ts
+++ test/presentation/controller/UpdateUserAccountController.spec.ts
@@ -4,29 +4,92 @@ import { UpdateUserAccount } from "@/domain/usecase/UpdateUserAccount"
 import {
   UpdateUserAccountController
 } from "@/presentation/controller/UpdateUserAccountController"
-import { Either, success } from "@/shared/Either"
+import { Either, failure, success } from "@/shared/Either"
+import { MissingParamError } from '@/presentation/error'
+
+const updateUserAccountStubFactory = (): UpdateUserAccount => {
+  class UpdateUserAccountStub implements UpdateUserAccount {
+    async execute(data: UpdateUserData): Promise<Either<InvalidParamError, void>> {
+      return success()
+    }
+  }
+  return new UpdateUserAccountStub()
+}
+
+type TypeSut = {
+  updateUserAccountStub: UpdateUserAccount
+  sut: UpdateUserAccountController
+}
+
+const makeSutFactory = (): TypeSut => {
+  const updateUserAccountStub = updateUserAccountStubFactory()
+  const sut = new UpdateUserAccountController(updateUserAccountStub)
+  return { sut, updateUserAccountStub }
+}
+
+const defaultUserDataParam = {
+  id: 'valid-id',
+  name: 'Any Name',
+  email: 'valid@email.com',
+  password: 'EncryptedPassword',
+  profileImage: 'path-image'
+}
 
 describe('UpdateUserAccountController', () => {
   test('should return statusCode 200 when update with success', async () => {
-    class UpdateUserAccountStub implements UpdateUserAccount {
-      async execute(data: UpdateUserData): Promise<Either<InvalidParamError, void>> {
-        return success()
-      }
-    }
-    const updateUserAccountStub = new UpdateUserAccountStub()
-    const sut = new UpdateUserAccountController(updateUserAccountStub)
-    const response = await sut.handle({
-      body: {
-        id: 'valid-id',
-        name: 'Any Name',
-        email: 'valid@email.com',
-        password: 'EncryptedPassword',
-        profileImage: 'path-image'
-      }
-    })
-
+    const { sut } = makeSutFactory()
+    const response = await sut.handle({ body: defaultUserDataParam })
     expect(response).toEqual({ statusCode: 200 })
   })
 
-  
+  test('should pass correct params to use case', async () => {
+    const { sut, updateUserAccountStub } = makeSutFactory()
+    const executeSpy = jest.spyOn(updateUserAccountStub, 'execute')
+
+    const response = await sut.handle({ body: defaultUserDataParam })
+    expect(response).toEqual({ statusCode: 200 })
+
+    expect(executeSpy).toBeCalledWith(defaultUserDataParam)
+  })
+
+  test('should return statusCode 422 and body error when id is undefined', async () => {
+    const { sut } = makeSutFactory()
+    const body = Object.assign({}, defaultUserDataParam)
+    delete body['id']
+
+    const response = await sut.handle({ body })
+
+    expect(response).toMatchObject({
+      statusCode: 422,
+      body: { message: "Attribute 'id' is invalid!" }
+    })
+  })
+
+
+  test('should return statusCode 422 and body error when email is invalid', async () => {
+    const { sut } = makeSutFactory()
+
+    const response = await sut.handle({ body: { ...defaultUserDataParam, email: 'invalid-email' } })
+
+    expect(response).toMatchObject({
+      statusCode: 422,
+      body: { message: "Attribute 'email' equals 'invalid-email' is invalid!" }
+    })
+  })
+
+
+  test('should return statusCode 400 and body error when use case return failure', async () => {
+    const { sut, updateUserAccountStub } = makeSutFactory()
+    jest.spyOn(updateUserAccountStub, 'execute').
+      mockImplementationOnce(() => Promise.resolve(failure(new Error('Any Error'))))
+
+    const response = await sut.handle({ body: defaultUserDataParam })
+
+    expect(response).toMatchObject({
+      statusCode: 500,
+      body: { message: "Internal server error" }
+    })
+  })
+
+
 })
