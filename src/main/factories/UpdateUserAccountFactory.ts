9d046d87cfab579bd3a407a339332dd47d69b851
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-15 13:54:05 -0300

    feat: create update user account factory

---
+++ src/main/factories/UpdateUserAccountFactory.ts
@@ -0,0 +1,40 @@
+import { DbUpdateUserAccount } from '@/data/usecase/DbUpdateUserAccount'
+import { PinoLoggerAdapter } from '@/infrastructure/adapter'
+import {
+  FindUserAccountByEmailORMRepository,
+  FindUserAccountByIdORMRepository,
+  UpdateUserAccountORMRepository
+} from '@/infrastructure/database/orm'
+
+import {
+  UpdateUserAccountController
+} from '@/presentation/controller/UpdateUserAccountController'
+import { Controller } from '@/presentation/protocols'
+import { ConnectionDatabaseFactory } from '@/main/factories/ConnectionDatabaseFactory'
+import { LogControllerDecorator } from '@/main/factories/LogControllerDecorator'
+import { EncryptsPasswordFactory } from '@/main/factories/EncryptsPasswordFactory'
+
+export class UpdateUserAccountFactory {
+  makeUpdateUserAccountFactory (): DbUpdateUserAccount {
+    const encryptsPassword = new EncryptsPasswordFactory().makeFactory()
+    const connection = new ConnectionDatabaseFactory().makeConnectionFactory()
+
+    const findUserAccountByEmailRepo = new FindUserAccountByEmailORMRepository(connection)
+    const findUserAccountByIdRepo = new FindUserAccountByIdORMRepository(connection)
+    const updateUserAccountRepo = new UpdateUserAccountORMRepository(connection)
+
+    return new DbUpdateUserAccount(
+      updateUserAccountRepo,
+      findUserAccountByEmailRepo,
+      findUserAccountByIdRepo,
+      encryptsPassword
+    )
+  }
+
+  makeControllerFactory = (): Controller => {
+    const logger = new PinoLoggerAdapter()
+    const updateUserAccount = this.makeUpdateUserAccountFactory()
+    const controller = new UpdateUserAccountController(updateUserAccount)
+    return new LogControllerDecorator(logger, controller)
+  }
+}
