8ad9424bfe8e6b0ce4ba5f6539c9017bcdcd9782
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-15 13:54:41 -0300

    feat: create update user account router

---
+++ src/main/express/routes/UpdateUserAccountRouter.ts
@@ -0,0 +1,10 @@
+import { UpdateUserAccountFactory } from '@/main/factories/UpdateUserAccountFactory'
+import { ExpressControllerAdapter } from '@/main/express/adapter/ExpressControllerAdapter'
+
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  const factory = new UpdateUserAccountFactory().makeControllerFactory()
+  const request = ExpressControllerAdapter(factory)
+  router.put('/account', request)
+}
