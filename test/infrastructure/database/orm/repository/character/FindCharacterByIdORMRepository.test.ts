430338056555d44973855aa29a794e6f87b31d2a
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-16 17:41:07 -0300

    test: implement find character by id repository

---
+++ test/infrastructure/database/orm/repository/character/FindCharacterByIdORMRepository.test.ts
@@ -0,0 +1,55 @@
+import {
+  EnvironmentConfiguration
+} from "@/infrastructure/util/EnvironmentConfiguration"
+import {
+  MySQLTypeOrmConnection
+} from "@/infrastructure/database/orm/connection/MySQLTypeOrmConnection"
+import { CharacterOrm } from "@/infrastructure/database/orm/model/CharacterOrm"
+import { FindCharacterByIdORMRepository } from "@/infrastructure/database/orm"
+
+const config = EnvironmentConfiguration.database()
+const connectionDatabase = new MySQLTypeOrmConnection(config)
+
+const defaultCharacterData = {
+  name: "Any Name",
+  description: "Any description",
+  topImage: "https://example.com/image-top-image.png",
+  profileImage: "https://example.com/image-profile-image.png",
+}
+
+describe('FindCharacterByIdORMRepository', () => {
+
+  beforeAll(async () => {
+    await connectionDatabase.open()
+  })
+
+  beforeEach(async () => {
+    await connectionDatabase.connection()
+      .createQueryBuilder()
+      .delete()
+      .from(CharacterOrm)
+      .execute()
+  })
+
+  afterAll(async () => await connectionDatabase.close())
+
+  test('should return success when found character by id', async () => {
+    const character = CharacterOrm.create(defaultCharacterData)
+
+    const characterInserted = await connectionDatabase.connection()
+      .createQueryBuilder().insert().into(CharacterOrm)
+      .values(character.value).execute()
+
+    const { id } = characterInserted.identifiers[0]
+
+    const sut = new FindCharacterByIdORMRepository(connectionDatabase)
+    const response = await sut.execute(id)
+
+    expect(response.isSuccess()).toBe(true)
+    expect(response.value).toEqual({
+      ...defaultCharacterData,
+      id, comic: undefined
+    })
+
+  })
+})
