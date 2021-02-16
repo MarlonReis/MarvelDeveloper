2f5c6a0223be7b4e9d66ccb6a229ac13ce70fed6
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-16 13:14:09 -0300

    feat: create character builder

--- src/domain/model/character/Character.ts
+++ src/domain/model/character/Character.ts
@@ -8,3 +8,39 @@ export interface Character {
   profileImage: string
   comics: Comic[]
 }
+
+export class CharacterBuilder {
+  private character: Character
+
+  private constructor () { }
+
+  public static build (character: Character): CharacterBuilder {
+    const build: CharacterBuilder = new CharacterBuilder()
+    build.character = character
+    return build
+  }
+
+  name (value: string): this {
+    this.character.name = value
+    return this
+  }
+
+  description (value: string): this {
+    this.character.description = value
+    return this
+  }
+
+  profileImage (value: string): this {
+    this.character.profileImage = value
+    return this
+  }
+
+  topImage (value: string): this {
+    this.character.topImage = value
+    return this
+  }
+
+  now (): Character {
+    return this.character
+  }
+}
