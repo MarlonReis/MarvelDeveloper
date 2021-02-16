ba09f3bbef65989a3c455020f4eecad74714326e
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-15 22:36:11 -0300

    feat: implement find character by id use case

---
+++ src/data/usecase/character/DbFindCharacterById.ts
@@ -0,0 +1,19 @@
+import { NotFoundError, RepositoryInternalError } from '@/data/error'
+import {
+  FindCharacterByIdRepository
+} from '@/data/repository/character/FindCharacterByIdRepository'
+import { CharacterResponse } from '@/domain/model/character/CharacterData'
+import { FindCharacterById } from '@/domain/usecase/character/FindCharacterById'
+import { Either } from '@/shared/Either'
+
+export class DbFindCharacterById implements FindCharacterById {
+  private readonly findCharacterByIdRepo: FindCharacterByIdRepository
+
+  constructor (repo: FindCharacterByIdRepository) {
+    this.findCharacterByIdRepo = repo
+  }
+
+  async execute (id: string): Promise<Either<NotFoundError | RepositoryInternalError, CharacterResponse>> {
+    return await this.findCharacterByIdRepo.execute(id)
+  }
+}
