8506b49a31fabdfc83278ec71383262d79e9db4b
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-16 13:14:48 -0300

    feat: create character data validation

--- src/domain/model/character/CharacterData.ts
+++ src/domain/model/character/CharacterData.ts
@@ -1,3 +1,7 @@
+import { InvalidParamError } from '@/domain/errors'
+import { Description, Name, PathFile } from '@/domain/value-object'
+import { Either } from '@/shared/Either'
+
 export interface CreateCharacterData {
   name: string
   description: string
@@ -12,4 +16,19 @@ export interface CharacterResponse {
   topImage: string
   profileImage: string
   comics: any[]
+}
+
+export const CharacterValidationData = {
+  name (value: string): Either<InvalidParamError, Name> {
+    return Name.create(value)
+  },
+  description (value: string): Either<InvalidParamError, Description> {
+    return Description.create(value)
+  },
+  profileImage (value: string): Either<InvalidParamError, PathFile> {
+    return PathFile.create('profileImage', value)
+  },
+  topImage (value: string): Either<InvalidParamError, PathFile> {
+    return PathFile.create('topImage', value)
+  }
 }
