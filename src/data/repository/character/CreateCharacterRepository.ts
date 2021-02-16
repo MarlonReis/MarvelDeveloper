34515f0eb99a2f8eefbf796b98c6457ebfe076c1
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-16 16:38:47 -0300

    refactor: create characters repository can return invalid param error

--- src/data/repository/character/CreateCharacterRepository.ts
+++ src/data/repository/character/CreateCharacterRepository.ts
@@ -1,7 +1,8 @@
-import { RepositoryInternalError } from '@/data/error'
 import { CreateCharacterData } from '@/domain/model/character/CharacterData'
+import { RepositoryInternalError } from '@/data/error'
+import { InvalidParamError } from '@/domain/errors'
 import { Either } from '@/shared/Either'
 
 export interface CreateCharacterRepository {
-  execute: (data: CreateCharacterData) => Promise<Either<RepositoryInternalError, void>>
+  execute: (data: CreateCharacterData) => Promise<Either<RepositoryInternalError | InvalidParamError, void>>
 }
