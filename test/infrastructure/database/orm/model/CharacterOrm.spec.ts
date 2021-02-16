d4e0593d4e88c8dab218aa7db9bd87718a3923b6
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-16 14:54:53 -0300

    refactor: change url path

--- test/infrastructure/database/orm/model/CharacterOrm.spec.ts
+++ test/infrastructure/database/orm/model/CharacterOrm.spec.ts
@@ -1,20 +1,33 @@
+import { InvalidParamError } from "@/domain/errors"
 import { CharacterOrm } from "@/infrastructure/database/orm/model/CharacterOrm"
 
+const defaultCharacterProp = {
+  name: 'Any Name',
+  description: 'Any Description',
+  topImage: 'https://anyurl.com/top-image.png',
+  profileImage: 'https://anyurl.com/profile-image.png'
+}
+
 describe('CharacterOrm', () => {
+ 
   test('should create character class instance', () => {
-    const response = CharacterOrm.create({
-      name: 'Any Name',
-      description: 'Any Description',
-      topImage: 'any path top image',
-      profileImage: 'any path profile image'
-    })
+    const response = CharacterOrm.create(defaultCharacterProp)
     expect(response.isSuccess()).toBe(true)
     expect(response.value).toBeInstanceOf(CharacterOrm)
-    expect(response.value).toEqual({
-      name: 'Any Name',
-      description: 'Any Description',
-      topImage: 'any path top image',
-      profileImage: 'any path profile image'
+    expect(response.value).toEqual(defaultCharacterProp)
+  })
+
+  test('should return failure when name is invalid', () => {
+    const response = CharacterOrm.create({
+      ...defaultCharacterProp,
+      name:'An'
+    })
+   
+    expect(response.isFailure()).toBe(true)
+    expect(response.value).toBeInstanceOf(InvalidParamError)
+    expect(response.value).toMatchObject({
+      message:"Attribute 'name' equals 'An' is invalid!"
     })
   })
+
 })
