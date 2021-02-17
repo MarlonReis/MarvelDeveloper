a962dbeb2e5a4d95fcc3bf873e3dfc8997248d98
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-17 19:21:31 -0300

    test: return 404 when not found register

--- test/main/express/routers/FindCharacterByIdRouter.test.ts
+++ test/main/express/routers/FindCharacterByIdRouter.test.ts
@@ -21,7 +21,6 @@ describe('FindCharacterByIdRouter', () => {
 
   beforeEach(async () => {
     await connectionDatabase.open()
-
     await connectionDatabase.connection().
       createQueryBuilder().delete().
       from(CharacterOrm).execute()
@@ -36,8 +35,29 @@ describe('FindCharacterByIdRouter', () => {
     await connectionDatabase.connection().
       createQueryBuilder().insert().into(CharacterOrm).
       values(defaultCharacterData).execute()
-    
+
     await request(app).get(`/api/characters/${defaultCharacterData.id}`).
       expect(200, defaultCharacterData)
   })
+
+  test('should return status 404 when not found register by id', async () => {
+    await request(app).get('/api/characters/id-not-exist').
+      expect(404, {
+        error: 'NotFoundError',
+        message: "Cannot found character by id equals 'id-not-exist'!"
+      })
+  })
+
+  test('should return status 500 when do internal error', async () => {
+    await connectionDatabase.close()
+
+    await request(app).get('/api/characters/id-not-exist').
+      expect(500, {
+        error: 'InternalServerError',
+        message: "Internal server error"
+      })
+  })
+
+
+
 })
