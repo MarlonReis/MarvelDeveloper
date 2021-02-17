90775525a6d47105264450fba20e3c2dc5965991
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-17 10:15:06 -0300

    feat: return failure when orm throws error

--- src/infrastructure/database/orm/repository/character/FindAllCharacterPageableORMRepository.ts
+++ src/infrastructure/database/orm/repository/character/FindAllCharacterPageableORMRepository.ts
@@ -2,8 +2,8 @@ import { ConnectionDatabase } from '@/infrastructure/database/protocol/Connectio
 import { CharacterOrm } from '@/infrastructure/database/orm/model/CharacterOrm'
 import { CharacterResponse } from '@/domain/model/character/CharacterData'
 import { Pagination, paginationParams, buildPagination } from '@/domain/helper/Pagination'
-import { Either, success } from '@/shared/Either'
-import { NotFoundError } from '@/data/error'
+import { Either, failure, success } from '@/shared/Either'
+import { NotFoundError, RepositoryInternalError } from '@/data/error'
 import {
   FindAllCharacterPageableRepository
 } from '@/data/repository/character/FindAllCharacterPageableRepository'
@@ -18,12 +18,14 @@ export class FindAllCharacterPageableORMRepository implements FindAllCharacterPa
   }
 
   async execute (page: number = 1, limit: number = 10): Promise<Either<NotFoundError, Pagination<CharacterResponse>>> {
-    const repository = this.connectionDatabase.connection().getRepository(CharacterOrm)
-
-    const count = await repository.count()
-    const { skip, take, maxPages, prevPage, nextPage } = paginationParams(page, limit, count)
-    const data = await repository.find({ skip, take })
-
-    return success(buildPagination(page, maxPages, count, prevPage, nextPage, data))
+    try {
+      const repository = this.connectionDatabase.connection().getRepository(CharacterOrm)
+      const count = await repository.count()
+      const { skip, take, maxPages, prevPage, nextPage } = paginationParams(page, limit, count)
+      const data = await repository.find({ skip, take })
+      return success(buildPagination(page, maxPages, count, prevPage, nextPage, data))
+    } catch (error) {
+      return failure(new RepositoryInternalError(error))
+    }
   }
 }
