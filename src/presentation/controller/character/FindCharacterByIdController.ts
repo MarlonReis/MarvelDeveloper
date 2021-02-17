b1962c89f6a178ca4aace4fbe3f11f20a0f3d6a2
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-17 18:24:59 -0300

    feat: return 400 when id is undefined

--- src/presentation/controller/character/FindCharacterByIdController.ts
+++ src/presentation/controller/character/FindCharacterByIdController.ts
@@ -3,6 +3,13 @@ import { IdEntity } from '@/domain/value-object'
 import { badRequest, internalServerError, ok } from '@/presentation/helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
+const isInvalidParam = (req: HttpRequest): boolean => {
+  if ('params' in req && 'id' in req.params) {
+    return IdEntity.create(req.params.id).isFailure()
+  }
+  return true
+}
+
 export class FindCharacterByIdController implements Controller {
   private readonly findCharacterById: FindCharacterById
 
@@ -11,12 +18,11 @@ export class FindCharacterByIdController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { id } = httpRequest.params
-
-    const characterId = IdEntity.create(id)
-    if (characterId.isFailure()) {
+    if (isInvalidParam(httpRequest)) {
       return badRequest("Attribute 'id' is invalid!")
     }
+
+    const { id } = httpRequest.params
 
     const response = await this.findCharacterById.execute(id)
     if (response.isSuccess()) {
