0057e8a52753a9328c64fd047c44c05d65e77730
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-18 16:58:09 -0300

    refactor: move validation to other file

--- src/infrastructure/database/orm/model/ComicOrm.ts
+++ src/infrastructure/database/orm/model/ComicOrm.ts
@@ -7,10 +7,14 @@ import {
 
 import { CharacterOrm } from './CharacterOrm'
 import { Comic } from '@/domain/model/comic/Comic'
-import { ComicBuilder, CreateComicData } from '@/domain/model/comic/ComicData'
+import {
+  ComicBuilder,
+  CreateComicData,
+  ComicValidationData
+} from '@/domain/model/comic/ComicData'
 import { InvalidParamError } from '@/domain/errors'
 import { Either, failure, success } from '@/shared/Either'
-import { Description, Name, PathFile, Published, Title } from '@/domain/value-object'
+import { Published } from '@/domain/value-object'
 
 @Entity('comics')
 export class ComicOrm implements Comic {
@@ -50,55 +54,22 @@ export class ComicOrm implements Comic {
   private constructor () { }
 
   static create (data: CreateComicData): Either<InvalidParamError, ComicOrm> {
-    const titleOrError = Title.create(data.title)
-    const writerOrError = Name.create(data.writer)
-    const pencilerOrError = Name.create(data.penciler)
-    const coverArtistOrError = Name.create(data.coverArtist)
-    const descriptionOrError = Description.create(data.description)
-    const coverImageOrError = PathFile.create('coverImage', data.coverImage)
-    const publishedOrError = Published.create(data.published)
+    const requiredFields = ['title', 'published', 'writer', 'penciler', 'coverArtist',
+      'description', 'edition', 'coverImage', 'characters']
 
-    if (titleOrError.isFailure()) {
-      return failure(titleOrError.value)
+    for (const field of requiredFields) {
+      const response = ComicValidationData[field](data[field])
+      if (response.isFailure()) {
+        return failure(response.value)
+      }
     }
+    const published: Published = Published.create(data.published).value as Published
 
-    if (writerOrError.isFailure()) {
-      return failure(writerOrError.value)
-    }
-
-    if (pencilerOrError.isFailure()) {
-      return failure(pencilerOrError.value)
-    }
-
-    if (coverArtistOrError.isFailure()) {
-      return failure(coverArtistOrError.value)
-    }
-
-    if (descriptionOrError.isFailure()) {
-      return failure(descriptionOrError.value)
-    }
-
-    if (coverImageOrError.isFailure()) {
-      return failure(coverImageOrError.value)
-    }
-
-    if (publishedOrError.isFailure()) {
-      return failure(publishedOrError.value)
-    }
-
-    const comic: ComicOrm = ComicBuilder
-      .build(new ComicOrm())
-      .title(titleOrError.value.value)
-      .writer(writerOrError.value.value)
-      .penciler(pencilerOrError.value.value)
-      .coverArtist(coverArtistOrError.value.value)
-      .description(descriptionOrError.value.value)
-      .coverImage(coverImageOrError.value.getValue())
-      .published(publishedOrError.value.value)
-      .edition(parseInt(data.edition))
-      .characters(data.characters as any[])
-      .now() as ComicOrm
-
-    return success(comic)
+    return success(ComicBuilder.build(new ComicOrm())
+      .title(data.title).writer(data.writer)
+      .penciler(data.penciler).coverArtist(data.coverArtist)
+      .description(data.description).coverImage(data.coverImage)
+      .published(published.value).edition(parseInt(data.edition))
+      .characters(data.characters as any[]).now() as ComicOrm)
   }
 }
