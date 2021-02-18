2513ae151cdd02adb07827c25a0dda5f4e816d71
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-18 14:02:49 -0300

    feat: create value object title

---
+++ src/domain/value-object/Title.ts
@@ -0,0 +1,22 @@
+import { Either, failure, success } from '@/shared/Either'
+import { InvalidParamError } from '@/domain/errors'
+
+export class Title {
+  public readonly value: string
+
+  private constructor (title: string) {
+      this.value = title
+      Object.freeze(this)
+  }
+
+  static create (title: string): Either<InvalidParamError, Title> {
+      if (title && /.{3,}$/.test(title)) {
+          return success(new Title(title))
+      }
+      return failure(new InvalidParamError('title', title))
+  }
+
+  getValue (): string {
+      return this.value
+  }
+}
