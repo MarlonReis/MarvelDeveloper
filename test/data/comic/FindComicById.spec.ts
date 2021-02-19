d68c728d441c283928789a5ab2f3988bc4439894
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-19 09:53:23 -0300

    test: return failure when repository return

--- test/data/comic/FindComicById.spec.ts
+++ test/data/comic/FindComicById.spec.ts
@@ -2,9 +2,9 @@ import { FindComicByIdRepository } from "@/data/repository/comic/FindComicByIdRe
 import { DbFindComicById } from "@/data/usecase/comic/DbFindComicById"
 import { NotFoundError, RepositoryInternalError } from "@/data/error"
 import { ComicResponse } from "@/domain/model/comic/ComicData"
-import { Either, success } from "@/shared/Either"
+import { Either, failure, success } from "@/shared/Either"
 
-const defaultComicResponse:ComicResponse = {
+const defaultComicResponse: ComicResponse = {
   id: 'valid-id',
   title: 'Any title',
   published: 'February 17, 2021',
@@ -25,22 +25,58 @@ const defaultComicResponse:ComicResponse = {
   ]
 }
 
+const findComicByIdRepoStubFactory = (): FindComicByIdRepository => {
+  class FindComicByIdRepoStub implements FindComicByIdRepository {
+    async execute(id: string): Promise<Either<NotFoundError | RepositoryInternalError, ComicResponse>> {
+      return success(defaultComicResponse)
+    }
+  }
+  return new FindComicByIdRepoStub()
+}
+
+type TypeSut = {
+  findComicByIdRepoStub: FindComicByIdRepository
+  sut: DbFindComicById
+}
+
+const makeSutFactory = (): TypeSut => {
+  const findComicByIdRepoStub = findComicByIdRepoStubFactory()
+  const sut = new DbFindComicById(findComicByIdRepoStub)
+  return { findComicByIdRepoStub, sut }
+}
+
 describe('FindComicById', () => {
   test('should return success when found register by id', async () => {
-    class FindComicByIdRepoStub implements FindComicByIdRepository {
-      async execute(id: string): Promise<Either<NotFoundError | RepositoryInternalError, ComicResponse>> {
-        return success(defaultComicResponse)
-      }
-    }
-    const findComicByIdRepoStub = new FindComicByIdRepoStub()
-    const sut = new DbFindComicById(findComicByIdRepoStub)
+    const { sut } = makeSutFactory()
+
     const response = await sut.execute("valid-id")
 
     expect(response.isSuccess()).toBe(true)
     expect(response.value).toHaveProperty('id', 'valid-id')
     expect(response.value).toHaveProperty('published', 'February 17, 2021')
     expect(response.value).toHaveProperty('coverImage', 'http://any-server.com/image.png')
-    expect(response.value).toHaveProperty('characters')
-
+    expect(response.value).toHaveProperty('characters', expect.any(Array))
   })
+
+  test('should call use case whe correct id', async () => {
+    const { sut, findComicByIdRepoStub } = makeSutFactory()
+    const executeSpy = jest.spyOn(findComicByIdRepoStub, 'execute')
+
+    await sut.execute("valid-id")
+
+    expect(executeSpy).toBeCalledWith('valid-id')
+  })
+
+  test('should return failure when repository return failure', async () => {
+    const { sut, findComicByIdRepoStub } = makeSutFactory()
+
+    jest.spyOn(findComicByIdRepoStub, 'execute')
+      .mockImplementationOnce(async () => failure(new NotFoundError('Any message')))
+
+    const response = await sut.execute("valid-id")
+
+    expect(response.isFailure()).toBe(true)
+    expect(response.value).toEqual(new NotFoundError('Any message'))
+  })
+
 })
