6fa782d58fdf9ef55f342827e74c1c050c6c59d8
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-19 15:15:44 -0300

    feat: validate  param id

--- src/presentation/controller/comic/FindComicByIdController.ts
+++ src/presentation/controller/comic/FindComicByIdController.ts
@@ -1,7 +1,15 @@
 import { NotFoundError } from '@/data/error'
 import { FindComicById } from '@/domain/usecase/comic/FindComicById'
-import { customError, internalServerError, ok } from '@/presentation/helper'
+import { IdEntity } from '@/domain/value-object'
+import { badRequest, customError, internalServerError, ok } from '@/presentation/helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+
+const isInvalidParam = (req: HttpRequest): boolean => {
+  if ('params' in req && 'id' in req.params) {
+    return IdEntity.create(req.params.id).isFailure()
+  }
+  return true
+}
 
 export class FindComicByIdController implements Controller {
   private readonly findComicById: FindComicById
@@ -11,9 +19,11 @@ export class FindComicByIdController implements Controller {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const { id } = request.params
+    if (isInvalidParam(request)) {
+      return badRequest("Attribute 'id' is invalid!")
+    }
 
-    const response = await this.findComicById.execute(id)
+    const response = await this.findComicById.execute(request.params.id)
     if (response.isSuccess()) {
       return ok(response.value)
     }
