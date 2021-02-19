8af1ab6c4bc017a1e40fe1d249f251f05f8e3bfb
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-19 15:16:09 -0300

    test: validate  param id

--- test/presentation/controller/comic/FindComicByIdController.spec.ts
+++ test/presentation/controller/comic/FindComicByIdController.spec.ts
@@ -1,8 +1,9 @@
 import { NotFoundError } from "@/data/error"
 import { ComicResponse } from "@/domain/model/comic/ComicData"
 import { FindComicById } from "@/domain/usecase/comic/FindComicById"
-import { Either, success } from "@/shared/Either"
+import { Either, failure, success } from "@/shared/Either"
 import { FindComicByIdController } from "@/presentation/controller/comic/FindComicByIdController"
+import { BadRequestError } from "@/presentation/error"
 
 const defaultComicData = {
   title: "Any Title",
@@ -43,4 +44,49 @@ describe('FindComicByIdController', () => {
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual(defaultComicData)
   })
+
+  test('should return statusCode 400 when para is undefined', async () => {
+    const { sut } = makeSutFactory()
+    const response = await sut.handle({})
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new BadRequestError("Attribute 'id' is invalid!"))
+  })
+
+  test('should call use case with correct param', async () => {
+    const { sut, findComicByIdStub } = makeSutFactory()
+
+    const executeSpy = jest.spyOn(findComicByIdStub, 'execute')
+
+    await sut.handle({ params: { id: 'valid-id' } })
+
+    expect(executeSpy).toBeCalledWith("valid-id")
+  })
+
+  test('should statusCode 404 when not found', async () => {
+    const { sut, findComicByIdStub } = makeSutFactory()
+
+    jest.spyOn(findComicByIdStub, 'execute').
+      mockImplementationOnce(async () => failure(new NotFoundError("Error message")))
+
+    const response = await sut.handle({ params: { id: 'valid-id' } })
+
+    expect(response.statusCode).toBe(404)
+    expect(response.body).toEqual(new NotFoundError("Error message"))
+  })
+
+
+  test('should statusCode 500 when use case return other error', async () => {
+    const { sut, findComicByIdStub } = makeSutFactory()
+
+    jest.spyOn(findComicByIdStub, 'execute').
+      mockImplementationOnce(async () => failure(new Error("Error message")))
+
+    const response = await sut.handle({ params: { id: 'valid-id' } })
+
+    expect(response.statusCode).toBe(500)
+    expect(response.body).toEqual(new Error("Internal server error"))
+  })
+
+
 })
