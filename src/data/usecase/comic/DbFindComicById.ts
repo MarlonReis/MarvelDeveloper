312c36f63a7e1da6af55ce2f3d320e1a2f60c6ab
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-19 09:13:16 -0300

    feat: implement data find comic by id use case

---
+++ src/data/usecase/comic/DbFindComicById.ts
@@ -0,0 +1,17 @@
+import { FindComicByIdRepository } from '@/data/repository/comic/FindComicByIdRepository'
+import { ComicResponse } from '@/domain/model/comic/ComicData'
+import { FindComicById } from '@/domain/usecase/comic/FindComicById'
+import { NotFoundError } from '@/data/error'
+import { Either } from '@/shared/Either'
+
+export class DbFindComicById implements FindComicById {
+  private readonly findComicByIdRepo: FindComicByIdRepository
+
+  constructor (repo: FindComicByIdRepository) {
+    this.findComicByIdRepo = repo
+  }
+
+  async execute (id: string): Promise<Either<NotFoundError, ComicResponse>> {
+    return await this.findComicByIdRepo.execute(id)
+  }
+}
