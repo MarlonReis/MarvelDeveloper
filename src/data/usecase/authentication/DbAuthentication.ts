65743a5688ddb2a05d37776aae666b855cbf4406
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-20 11:10:10 -0300

    feat: generate token when receive correct param

--- src/data/usecase/authentication/DbAuthentication.ts
+++ src/data/usecase/authentication/DbAuthentication.ts
@@ -1,17 +1,24 @@
 import { NotFoundError } from '@/data/error'
 import { ComparePassword } from '@/data/protocol/ComparePassword'
+import { TokenGenerator } from '@/data/protocol/TokenGenerator'
 import { FindUserAccountByEmailRepository } from '@/data/repository/user/FindUserAccountByEmailRepository'
 import { AuthData } from '@/domain/model/user/AuthenticationData'
 import { Authentication } from '@/domain/usecase/authentication/Authentication'
-import { Either, failure, success } from '@/shared/Either'
+import { Either, failure } from '@/shared/Either'
 
 export class DbAuthentication implements Authentication {
   private readonly findByEmailRepo: FindUserAccountByEmailRepository
   private readonly comparePassword: ComparePassword
+  private readonly tokenGenerator: TokenGenerator
 
-  constructor (findByEmailRepo: FindUserAccountByEmailRepository, comparePassword: ComparePassword) {
+  constructor (
+    findByEmailRepo: FindUserAccountByEmailRepository,
+    comparePassword: ComparePassword,
+    tokenGenerator: TokenGenerator
+  ) {
     this.findByEmailRepo = findByEmailRepo
     this.comparePassword = comparePassword
+    this.tokenGenerator = tokenGenerator
   }
 
   async execute (auth: AuthData): Promise<Either<NotFoundError, string>> {
@@ -19,7 +26,7 @@ export class DbAuthentication implements Authentication {
     if (response.isSuccess()) {
       const comparePwd = await this.comparePassword.execute(auth.password, response.value.password)
       if (comparePwd.isSuccess()) {
-        return success('ok')
+        return await this.tokenGenerator.execute(response.value.id)
       }
       return failure(comparePwd.value)
     }
