13c7703712de9560e81e3f1491d1c1c4be75b443
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-20 20:35:58 -0300

    test: implement jwt decrypt token

---
+++ test/infrastructure/adapter/JwtDecryptAuthTokenAdapter.spec.ts
@@ -0,0 +1,55 @@
+import jwt from 'jsonwebtoken'
+
+import {
+  JwtDecryptAuthTokenAdapter
+} from '@/infrastructure/adapter/JwtDecryptAuthTokenAdapter'
+import { DecryptError } from '@/data/error'
+
+jest.mock('jsonwebtoken', () => ({
+  async decode(): Promise<any> {
+    return {
+      payload: {
+        id: 'valid_id'
+      }
+
+    }
+  }
+}))
+
+describe('JwtDecryptAuthTokenAdapter', () => {
+  test('should call jwt decode with correct values', async () => {
+    const decodeSpy = jest.spyOn(jwt, 'decode')
+
+    const sut = new JwtDecryptAuthTokenAdapter()
+    await sut.execute("valid_token")
+    expect(decodeSpy).toHaveBeenCalledWith("valid_token", expect.anything())
+  })
+
+  test('should failure when not have id in payload', async () => {
+    const sut = new JwtDecryptAuthTokenAdapter()
+    const response = await sut.execute("valid_token")
+
+    expect(response.value).toEqual("valid_id")
+  })
+
+  test('should return failure when decode return undefined', async () => {
+    jest.spyOn(jwt, 'decode').mockImplementationOnce(() => undefined)
+
+    const sut = new JwtDecryptAuthTokenAdapter()
+    const response = await sut.execute("valid_token")
+
+    expect(response.value).toEqual(new DecryptError('Unable to decrypt token'))
+  })
+
+  test('should return failure when decode throws error', async () => {
+    jest.spyOn(jwt, 'decode').mockImplementationOnce(() => {
+      throw new Error('Error message')
+    })
+
+    const sut = new JwtDecryptAuthTokenAdapter()
+    const response = await sut.execute("valid_token")
+
+    expect(response.value).toEqual(new DecryptError('Error message'))
+  })
+
+})
