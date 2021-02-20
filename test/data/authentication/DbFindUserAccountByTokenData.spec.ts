4c7f002c378642f46eca50ee1e1f933b620ca26e
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-19 21:12:29 -0300

    refactor: move sut creation to factory method

--- test/data/authentication/DbFindUserAccountByTokenData.spec.ts
+++ test/data/authentication/DbFindUserAccountByTokenData.spec.ts
@@ -6,20 +6,31 @@ import {
 } from "@/data/usecase/authentiction/DbFindUserAccountByTokenData"
 import { Role } from "@/domain/model/user/AuthenticationData"
 
+const decryptAuthTokenStubFactory = (): DecryptAuthToken => {
+  class DecryptAuthTokenStub implements DecryptAuthToken {
+    async execute(data: string): Promise<Either<DecryptError, string>> {
+      return success("token_decrypted")
+    }
+  }
+  return new DecryptAuthTokenStub()
+}
+
+type TypeSut = {
+  decryptAuthTokenStub: DecryptAuthToken
+  sut: DbFindUserAccountByTokenData
+}
+
+const makeSutFactory = (): TypeSut => {
+  const decryptAuthTokenStub = decryptAuthTokenStubFactory()
+  const sut = new DbFindUserAccountByTokenData(decryptAuthTokenStub)
+  return { decryptAuthTokenStub, sut }
+}
+
 describe('DbFindUserAccountByTokenData', () => {
   test('should call decrypt token with correct values', async () => {
-
-    class DecryptAuthTokenStub implements DecryptAuthToken {
-      async execute(data: string): Promise<Either<DecryptError, string>> {
-        return success("token_decrypted")
-      }
-    }
-
-    const decryptAuthTokenStub = new DecryptAuthTokenStub()
-
+    const { sut, decryptAuthTokenStub } = makeSutFactory()
     const executeSpy = jest.spyOn(decryptAuthTokenStub, 'execute')
 
-    const sut = new DbFindUserAccountByTokenData(decryptAuthTokenStub)
     await sut.execute('valid-token', Role.USER)
 
     expect(executeSpy).toHaveBeenCalledWith('valid-token')
