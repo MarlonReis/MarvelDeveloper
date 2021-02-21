61a7101d87b35e2e619df55c8e7b361acbe7da23
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-21 15:50:06 -0300

    refactor: move class creation to factory method

--- test/data/user/DbUserFavoriteComic.spec.ts
+++ test/data/user/DbUserFavoriteComic.spec.ts
@@ -8,28 +8,46 @@ import { RepositoryInternalError } from "@/data/error"
 import { Either, success } from "@/shared/Either"
 
 
+const userFavoriteRepoStubFactory = (): UserFavoriteComicRepository => {
+  class UserFavoriteRepoStub implements UserFavoriteComicRepository {
+    async favoriteComic(data: FavoriteComicData): Promise<Either<InvalidParamError, void>> {
+      return success()
+    }
+  }
+  return new UserFavoriteRepoStub()
+}
+
+const findCharacterByIdRepoStubFactory = (): FindCharacterByIdRepository => {
+  class FindCharacterByIdRepoStub implements FindCharacterByIdRepository {
+    async execute(id: string): Promise<Either<NotFoundError | RepositoryInternalError, CharacterResponse>> {
+      return success({ id: 'valid-id' } as CharacterResponse)
+    }
+  }
+  return new FindCharacterByIdRepoStub()
+}
+
+type TypeSut = {
+  findCharacterByIdRepoStub: FindCharacterByIdRepository
+  userFavoriteRepoStub: UserFavoriteComicRepository
+  sut: DbUserFavoriteComic
+}
+
+const makeSutFactory = (): TypeSut => {
+  const findCharacterByIdRepoStub = findCharacterByIdRepoStubFactory()
+  const userFavoriteRepoStub = userFavoriteRepoStubFactory()
+
+  const sut = new DbUserFavoriteComic(userFavoriteRepoStub, findCharacterByIdRepoStub)
+  return { findCharacterByIdRepoStub, userFavoriteRepoStub, sut }
+}
+
+
 describe('DbUserFavoriteComic', () => {
   test('should return success when favorite comic', async () => {
-    class UserFavoriteRepoStub implements UserFavoriteComicRepository {
-      async favoriteComic(data: FavoriteComicData): Promise<Either<InvalidParamError, void>> {
-        return success()
-      }
-    }
-
-    class FindCharacterByIdRepoStub implements FindCharacterByIdRepository {
-      async execute(id: string): Promise<Either<NotFoundError | RepositoryInternalError, CharacterResponse>> {
-        return success({ id: 'valid-id' } as CharacterResponse)
-      }
-    }
-    const findCharacterByIdRepoStub = new FindCharacterByIdRepoStub()
-    const userFavoriteRepoStub = new UserFavoriteRepoStub()
-    
-    const sut = new DbUserFavoriteComic(userFavoriteRepoStub, findCharacterByIdRepoStub)
+    const { sut } = makeSutFactory()
     const response = await sut.execute({
       comicId: 'comic_valid_id',
       userId: 'user_valid_id'
     })
-
     expect(response.isSuccess()).toBe(true)
   })
 })
