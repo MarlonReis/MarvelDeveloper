93eaa6415edb5f64f8dc2117986701ea33c4c5c3
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-21 13:00:58 -0300

    feat: add auth middleware in post character

--- src/main/express/routes/CreateCharacterRouter.ts
+++ src/main/express/routes/CreateCharacterRouter.ts
@@ -2,9 +2,13 @@ import { CreateCharacterFactory } from '@/main/factories/character/CreateCharact
 import { ExpressControllerAdapter } from '@/main/express/adapter/ExpressControllerAdapter'
 
 import { Router } from 'express'
+import { AuthenticationMiddleware } from '@/main/factories/middlewares/AuthenticationMiddleware'
+import { Role } from '@/domain/model/user/AuthenticationData'
 
 export default (router: Router): void => {
+  const authMiddleware = new AuthenticationMiddleware().makeMiddlewareFactory(Role.ADMIN)
+
   const factory = new CreateCharacterFactory().makeControllerFactory()
   const request = ExpressControllerAdapter(factory)
-  router.post('/characters', request)
+  router.post('/characters', authMiddleware, request)
 }
