823ce09054c7f97c83219d4390bfb9d382a918f5
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-20 22:55:47 -0300

    feat: add role in user

--- src/domain/model/user/User.ts
+++ src/domain/model/user/User.ts
@@ -1,9 +1,11 @@
+import { Role } from './AuthenticationData'
 import { StatusUser } from './StatusUser'
 
 export interface User {
   id?: string
   name: string
   email: string
+  role: Role
   password: string
   status: StatusUser
   comicsReactions?: any[]
