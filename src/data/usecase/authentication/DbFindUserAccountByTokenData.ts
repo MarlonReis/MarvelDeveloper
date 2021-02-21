506b7c883d0de68a005d8c590831a00d3daaa1b2
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-20 23:00:41 -0300

    refactor: change repository

--- src/data/usecase/authentication/DbFindUserAccountByTokenData.ts
+++ src/data/usecase/authentication/DbFindUserAccountByTokenData.ts
@@ -1,26 +1,29 @@
 import { DecryptError, NotFoundError } from '@/data/error'
 import { DecryptAuthToken } from '@/data/protocol/DecryptAuthToken'
-import { FindUserAccountByTokenDataRepository } from '@/data/repository/authentication/FindUserAccountByTokenDataRepository'
-
+import { FindUserAccountByIdRepository } from '@/data/repository/user/FindUserAccountByIdRepository'
 import { Role, AuthResponse } from '@/domain/model/user/AuthenticationData'
 import {
   FindUserAccountByTokenData
 } from '@/domain/usecase/authentication/FindUserAccountByTokenData'
-import { Either, failure } from '@/shared/Either'
+import { Either, failure, success } from '@/shared/Either'
 
 export class DbFindUserAccountByTokenData implements FindUserAccountByTokenData {
-  private readonly findByTokenDataRepo: FindUserAccountByTokenDataRepository
+  private readonly findUserAccountByIdRepo: FindUserAccountByIdRepository
   private readonly decryptAuthToken: DecryptAuthToken
 
-  constructor (decryptAuthToken: DecryptAuthToken, findByTokenDataRepo: FindUserAccountByTokenDataRepository) {
+  constructor (decryptAuthToken: DecryptAuthToken, findUserAccountByIdRepo: FindUserAccountByIdRepository) {
     this.decryptAuthToken = decryptAuthToken
-    this.findByTokenDataRepo = findByTokenDataRepo
+    this.findUserAccountByIdRepo = findUserAccountByIdRepo
   }
 
   async execute (token: string, role: Role): Promise<Either<NotFoundError | DecryptError, AuthResponse>> {
     const responseToken = await this.decryptAuthToken.execute(token)
     if (responseToken.isSuccess()) {
-      return await this.findByTokenDataRepo.execute(responseToken.value, role)
+      const response = await this.findUserAccountByIdRepo.execute(responseToken.value)
+      if (response.isSuccess()) {
+        return success({ id: response.value.id })
+      }
+      return failure(response.value)
     }
     return failure(responseToken.value)
   }
