67be5c5f05065cb0f1237b3a1d10a476f41937f0
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-21 13:01:58 -0300

    test: add auth middleware in post character

--- test/main/express/routers/CreateCharacterRouter.test.ts
+++ test/main/express/routers/CreateCharacterRouter.test.ts
@@ -4,6 +4,8 @@ import {
   ConnectionDatabaseFactory
 } from '@/main/factories/ConnectionDatabaseFactory'
 import { CharacterOrm } from '@/infrastructure/database/orm/model/CharacterOrm'
+import { deleteAllUserMock, insertUserMock } from './InsertUserMock'
+import { Role } from '@/domain/model/user/AuthenticationData'
 
 
 const connectionDatabase = new ConnectionDatabaseFactory()
@@ -16,12 +18,27 @@ const defaultRequestBody = {
   profileImage: 'https://anyserver.com/profileImage.png',
 }
 
-const postRequest = request(app).post('/api/characters')
-
 describe('CreateCharacterRouter', () => {
+  let tokenAdmin: string
+
   beforeEach(async () => {
     await connectionDatabase.open()
+
+    const { token } = await insertUserMock(connectionDatabase, {
+      name: 'Any Name',
+      email: 'any@valid.com',
+      password: 'Password@Valid',
+      role: Role.ADMIN
+    })
+
+    tokenAdmin = token.value
   })
+
+  afterEach(async () => {
+    if (connectionDatabase.connection())
+      await deleteAllUserMock(connectionDatabase)
+  })
+
 
   afterAll(async () => {
     await connectionDatabase.connection().createQueryBuilder().delete()
@@ -30,25 +47,55 @@ describe('CreateCharacterRouter', () => {
     await connectionDatabase.close()
   })
 
+  test('should return statusCode 401 when not have token', async () => {
+    await request(app).
+      post('/api/characters').
+      send(defaultRequestBody).
+      expect(401)
+  })
+
+  test('should return statusCode 403 when use not have permission', async () => {
+    const { token } = await insertUserMock(connectionDatabase, {
+      name: 'Any Name',
+      email: 'any@valid.com',
+      password: 'Password@Valid',
+      role: Role.USER
+    })
+
+    await request(app).
+      post('/api/characters').
+      set({ 'Authentication': `Bearer ${token.value}` }).
+      send(defaultRequestBody).
+      expect(403)
+  })
+
+
   test('should return statusCode 200 when it`s has success', async () => {
-    await postRequest.send(defaultRequestBody).expect(201)
+    await request(app).post('/api/characters').
+      set({ 'Authentication': `Bearer ${tokenAdmin}` }).
+      send(defaultRequestBody).expect(201)
   })
 
   test('should return statusCode 422 when name is invalid', async () => {
-    await postRequest.send({
-      ...defaultRequestBody,
-      name: 'in'
-    }).expect(422, {
-      message: "Attribute 'name' is invalid!",
-      error: 'MissingParamError'
-    })
+    await request(app).post('/api/characters').
+      set({ 'Authentication': `Bearer ${tokenAdmin}` }).
+      send({
+        ...defaultRequestBody,
+        name: 'in'
+      }).expect(422, {
+        message: "Attribute 'name' is invalid!",
+        error: 'MissingParamError'
+      })
   })
 
   test('should return statusCode 500 when internal error', async () => {
     await connectionDatabase.close()
-    await postRequest.send(defaultRequestBody).expect(500, {
-      error: 'InternalServerError',
-      message: 'Internal server error'
-    })
+    await request(app).post('/api/characters').
+      set({ 'Authentication': `Bearer ${tokenAdmin}` })
+      .send(defaultRequestBody).expect(500, {
+        error: 'InternalServerError',
+        message: 'Internal server error'
+      })
   })
+
 })
