9b3d5a2a53aeba9147cdb3ed0f4636911cf404c2
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-21 14:42:38 -0300

    test: add favorite comics

--- test/infrastructure/database/orm/model/UserOrm.spec.ts
+++ test/infrastructure/database/orm/model/UserOrm.spec.ts
@@ -1,5 +1,6 @@
 import { StatusUser } from '@/domain/model/user/StatusUser'
 import { UserOrm } from '@/infrastructure/database/orm/model/UserOrm'
+import { ComicOrm } from './ComicOrm'
 
 describe('UserOrm', () => {
   test('should create user instance with success', () => {
@@ -102,6 +103,65 @@ describe('UserOrm', () => {
     expect(response.isSuccess()).toBe(false)
     expect(response.value).toMatchObject({
       message: "Attribute 'email' equals 'invalid-email' is invalid!"
-    })    
+    })
   })
+
+
+  test('should return new favorite list of Comics', () => {
+    const myFavorites = [
+      { id: 'valid_id_a', title: 'valid_title' },
+      { id: 'valid_id_b', title: 'valid_title' }
+    ] as ComicOrm[]
+
+    const newComics = [{ id: 'valid_id_c', title: 'valid_title' }] as ComicOrm[]
+
+    const response = UserOrm.doFavoriteComics(myFavorites, newComics)
+
+    expect(response).toEqual(expect.arrayContaining(newComics))
+    expect(response).toHaveLength(3)
+  })
+
+  test('should remove duplicate', () => {
+    const myFavorites = [
+      { id: 'valid_id_duplicate', title: 'valid_title' },
+      { id: 'valid_id_b', title: 'valid_title' }
+    ] as ComicOrm[]
+
+    const newComics = [
+      { id: 'valid_id_duplicate', title: 'valid_title' },
+      { id: 'valid_id_c', title: 'valid_title' }
+    ] as ComicOrm[]
+
+    const response = UserOrm.doFavoriteComics(myFavorites, newComics)
+    expect(response).toEqual(expect.arrayContaining(newComics))
+    expect(response).toHaveLength(3)
+  })
+
+
+  test('should add my first favorite', () => {
+    const myFavorites = undefined
+
+    const newComics = [
+      { id: 'valid_id_duplicate', title: 'valid_title' },
+      { id: 'valid_id_c', title: 'valid_title' }
+    ] as ComicOrm[]
+
+    const response = UserOrm.doFavoriteComics(myFavorites, newComics)
+
+    expect(response).toEqual(expect.arrayContaining(newComics))
+    expect(response).toHaveLength(2)
+  })
+
+  test('should return same list when try add undefined', () => {
+    const myFavorites = [
+      { id: 'valid_id_duplicate', title: 'valid_title' },
+      { id: 'valid_id_c', title: 'valid_title' }
+    ] as ComicOrm[]
+
+    const response = UserOrm.doFavoriteComics(myFavorites, undefined)
+
+    expect(response).toEqual(expect.arrayContaining(myFavorites))
+    expect(response).toHaveLength(2)
+  })
+
 })
