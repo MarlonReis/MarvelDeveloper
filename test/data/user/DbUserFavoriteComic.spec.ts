58b9c52438475c0d6a91af27b3964f975c12fb54
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-21 16:19:05 -0300

    test: user favorite comic

--- test/data/user/DbUserFavoriteComic.spec.ts
+++ test/data/user/DbUserFavoriteComic.spec.ts
@@ -1,11 +1,12 @@
 import { FindCharacterByIdRepository } from "@/data/repository/character/FindCharacterByIdRepository"
 import { UserFavoriteComicRepository } from "@/data/repository/user/UserFavoriteComicRepository"
 import { DbUserFavoriteComic } from "@/data/usecase/user/DbUserFavoriteComic"
-import { CharacterResponse } from "@/domain/model/character/CharacterData"
 import { InvalidParamError, NotFoundError } from "@/domain/errors"
 import { FavoriteComicData } from "@/domain/model/user/UserData"
 import { RepositoryInternalError } from "@/data/error"
-import { Either, success } from "@/shared/Either"
+import { Either, failure, success } from "@/shared/Either"
+import { FindComicByIdRepository } from "../repository/comic/FindComicByIdRepository"
+import { ComicResponse } from "@/domain/model/comic/ComicData"
 
 
 const userFavoriteRepoStubFactory = (): UserFavoriteComicRepository => {
@@ -17,37 +18,88 @@ const userFavoriteRepoStubFactory = (): UserFavoriteComicRepository => {
   return new UserFavoriteRepoStub()
 }
 
-const findCharacterByIdRepoStubFactory = (): FindCharacterByIdRepository => {
-  class FindCharacterByIdRepoStub implements FindCharacterByIdRepository {
-    async execute(id: string): Promise<Either<NotFoundError | RepositoryInternalError, CharacterResponse>> {
-      return success({ id: 'valid-id' } as CharacterResponse)
+const findComicByIdRepoStubFactory = (): FindComicByIdRepository => {
+  class FindComicByIdRepoStub implements FindComicByIdRepository {
+    async execute(id: string): Promise<Either<NotFoundError | RepositoryInternalError, ComicResponse>> {
+      return success({
+        id: 'id_Valid',
+        title: 'Any title'
+      } as ComicResponse)
     }
+
+
   }
-  return new FindCharacterByIdRepoStub()
+  return new FindComicByIdRepoStub()
 }
 
 type TypeSut = {
-  findCharacterByIdRepoStub: FindCharacterByIdRepository
+  findComicByIdRepoStub: FindComicByIdRepository
   userFavoriteRepoStub: UserFavoriteComicRepository
   sut: DbUserFavoriteComic
 }
 
 const makeSutFactory = (): TypeSut => {
-  const findCharacterByIdRepoStub = findCharacterByIdRepoStubFactory()
+  const findComicByIdRepoStub = findComicByIdRepoStubFactory()
   const userFavoriteRepoStub = userFavoriteRepoStubFactory()
 
-  const sut = new DbUserFavoriteComic(userFavoriteRepoStub, findCharacterByIdRepoStub)
-  return { findCharacterByIdRepoStub, userFavoriteRepoStub, sut }
+  const sut = new DbUserFavoriteComic(userFavoriteRepoStub, findComicByIdRepoStub)
+  return { findComicByIdRepoStub, userFavoriteRepoStub, sut }
 }
+
+const fakeArgument = (): FavoriteComicData => ({
+  comicId: 'comic_valid_id',
+  userId: 'user_valid_id'
+})
 
 
 describe('DbUserFavoriteComic', () => {
   test('should return success when favorite comic', async () => {
     const { sut } = makeSutFactory()
-    const response = await sut.execute({
-      comicId: 'comic_valid_id',
-      userId: 'user_valid_id'
-    })
+    const response = await sut.execute(fakeArgument())
     expect(response.isSuccess()).toBe(true)
   })
+
+  test('should call find character by id with correct id', async () => {
+    const { sut, findComicByIdRepoStub } = makeSutFactory()
+    const executeSpy = jest.spyOn(findComicByIdRepoStub, 'execute')
+
+    await sut.execute(fakeArgument())
+
+    expect(executeSpy).toHaveBeenCalledWith('comic_valid_id')
+  })
+
+
+  test('should call favorite comic with params', async () => {
+    const { sut, userFavoriteRepoStub } = makeSutFactory()
+    const favoriteComicSpy = jest.spyOn(userFavoriteRepoStub, 'favoriteComic')
+
+    await sut.execute(fakeArgument())
+
+    expect(favoriteComicSpy).toHaveBeenCalledWith(fakeArgument())
+  })
+
+
+  test('should return failure when not found comic', async () => {
+    const { sut, findComicByIdRepoStub } = makeSutFactory()
+
+    jest.spyOn(findComicByIdRepoStub, 'execute').
+      mockImplementationOnce(async () => failure(new Error('Any error')))
+
+    const response = await sut.execute(fakeArgument())
+
+    expect(response.isFailure()).toBe(true)
+  })
+
+  test('should return failure when not found comic', async () => {
+    const { sut, userFavoriteRepoStub } = makeSutFactory()
+
+    jest.spyOn(userFavoriteRepoStub, 'favoriteComic').
+      mockImplementationOnce(async () => failure(new Error('Any error')))
+
+    const response = await sut.execute(fakeArgument())
+
+    expect(response.isFailure()).toBe(true)
+  })
+
+
 })
