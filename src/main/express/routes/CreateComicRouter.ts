a1e01110f25b818190eee9e8c212aaa613db475e
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-21 12:39:10 -0300

    feat: add admin auth middleware in comic router

--- src/main/express/routes/CreateComicRouter.ts
+++ src/main/express/routes/CreateComicRouter.ts
@@ -1,10 +1,14 @@
+import { Role } from '@/domain/model/user/AuthenticationData'
 import { ExpressControllerAdapter } from '@/main/express/adapter/ExpressControllerAdapter'
 import { CreateComicFactory } from '@/main/factories/comic/CreateComicFactory'
+import { AuthenticationMiddleware } from '@/main/factories/middlewares/AuthenticationMiddleware'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
+  const authMiddleware = new AuthenticationMiddleware().makeMiddlewareFactory(Role.ADMIN)
+
   const factory = new CreateComicFactory().makeControllerFactory()
   const request = ExpressControllerAdapter(factory)
-  router.post('/comics', request)
+  router.post('/comics', authMiddleware, request)
 }
