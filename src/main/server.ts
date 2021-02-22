dd7eb4a7740caf7cb002162350541ebd2647f4d4
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-22 12:39:01 -0300

    fix: exit process when not open database connection

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,10 +3,18 @@ import 'module-alias/register'
 import { EnvironmentConfiguration } from '@/infrastructure/util/EnvironmentConfiguration'
 import { ConnectionDatabaseFactory } from './factories/ConnectionDatabaseFactory'
 import app from '@/main/express/config/App'
+import { PinoLoggerAdapter } from '@/infrastructure/adapter'
 
-const appPort = EnvironmentConfiguration.appPort()
+(async () => {
+  const logger = new PinoLoggerAdapter()
 
-const connection = new ConnectionDatabaseFactory().makeConnectionFactory()
-connection.open().then(() => {
-  app.listen(appPort, () => console.log(`🚀 Server running at http://localhost:${appPort}`))
-})
+  const appPort = EnvironmentConfiguration.appPort()
+  const connection = new ConnectionDatabaseFactory().makeConnectionFactory()
+  const response = await connection.open()
+  if (response.isSuccess()) {
+    app.listen(appPort, () => logger.info('server', `🚀 Server running at http://localhost:${appPort}`))
+  } else {
+    logger.error('server', response.value)
+    process.exit(1)
+  }
+})()
