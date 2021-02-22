9b9a33846a95696b83af236dddae11236ff3b36a
Marlon Reis <marlon-reis91@hotmail.com>
2021-02-22 14:33:12 -0300

    test: ensure that application get correct variables

--- src/infrastructure/util/EnvironmentConfiguration.ts
+++ src/infrastructure/util/EnvironmentConfiguration.ts
@@ -2,15 +2,44 @@ import * as dotenv from 'dotenv'
 
 import { ConfigConnection } from '@/infrastructure/database/protocol/ConnectionDatabase'
 
-export const EnvironmentConfiguration = {
+enum Environment {
+  LOCAL = 'local',
+  TEST = 'test'
+}
+
+const getDatabaseLocal = (): ConfigConnection => {
+  const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD } = dotenv.config().parsed
+  return ({
+    host: DB_HOST,
+    port: parseInt(DB_PORT || '3306'),
+    username: DB_USERNAME,
+    password: DB_PASSWORD
+  })
+}
+
+const getDatabaseTest = (): ConfigConnection => {
+  const { TEST_DB_HOST, TEST_DB_PORT, TEST_DB_USERNAME, TEST_DB_PASSWORD } = dotenv.config().parsed
+  return ({
+    host: TEST_DB_HOST,
+    port: parseInt(TEST_DB_PORT || '3306'),
+    username: TEST_DB_USERNAME,
+    password: TEST_DB_PASSWORD
+  })
+}
+
+const EnvironmentConfiguration = {
+  environment (): Environment {
+      if (process.env.NODE_ENV.trim() === Environment.LOCAL) {
+      return Environment.LOCAL
+    }
+    return Environment.TEST
+  },
+
   database (): ConfigConnection {
-    const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD } = dotenv.config().parsed
-    return ({
-      host: DB_HOST,
-      port: parseInt(DB_PORT || '3306'),
-      username: DB_USERNAME,
-      password: DB_PASSWORD
-    })
+    if (EnvironmentConfiguration.environment() === Environment.LOCAL) {
+      return getDatabaseLocal()
+    }
+    return getDatabaseTest()
   },
 
   authenticationSecretKey (): string {
@@ -19,8 +48,13 @@ export const EnvironmentConfiguration = {
   },
 
   appPort (): number {
-    const { APP_PORT } = dotenv.config().parsed
+    const { APP_PORT, TEST_APP_PORT } = dotenv.config().parsed
+    if (EnvironmentConfiguration.environment() === Environment.TEST) {
+      return parseInt(TEST_APP_PORT)
+    }
     return parseInt(APP_PORT)
   }
 
 }
+
+export { EnvironmentConfiguration, Environment }
--- test/infrastructure/util/EnvironmentConfiguration.test.ts
+++ test/infrastructure/util/EnvironmentConfiguration.test.ts
@@ -1,11 +1,25 @@
-import { EnvironmentConfiguration } from '@/infrastructure/util/EnvironmentConfiguration'
+import { EnvironmentConfiguration, Environment } from '@/infrastructure/util/EnvironmentConfiguration'
 
 import dotenv from 'dotenv'
 
 describe('EnvironmentConfiguration', () => {
   test('should return database configuration', () => {
-    const response = EnvironmentConfiguration.database()
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.TEST)
 
+
+    const response = EnvironmentConfiguration.database()
+    expect(response).toHaveProperty('host')
+    expect(response).toHaveProperty('port')
+    expect(response).toHaveProperty('username')
+    expect(response).toHaveProperty('password')
+  })
+
+  test('should return database configuration local', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.LOCAL)
+
+    const response = EnvironmentConfiguration.database()
     expect(response).toHaveProperty('host')
     expect(response).toHaveProperty('port')
     expect(response).toHaveProperty('username')
@@ -13,6 +27,9 @@ describe('EnvironmentConfiguration', () => {
   })
 
   test('should ensure return same environment variable', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.LOCAL)
+
     jest.spyOn(dotenv, 'config').mockReturnValueOnce({
       parsed: {
         DB_HOST: 'Any Host',
@@ -30,7 +47,31 @@ describe('EnvironmentConfiguration', () => {
     })
   })
 
+  test('should ensure return same environment variable  when environment is test', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.TEST)
+
+    jest.spyOn(dotenv, 'config').mockReturnValueOnce({
+      parsed: {
+        TEST_DB_HOST: 'Any Host',
+        TEST_DB_PORT: '1007',
+        TEST_DB_USERNAME: 'Any Name',
+        TEST_DB_PASSWORD: 'Any Password'
+      }
+    })
+    const response = EnvironmentConfiguration.database()
+    expect(response).toMatchObject({
+      host: 'Any Host',
+      port: 1007,
+      username: 'Any Name',
+      password: 'Any Password'
+    })
+  })
+
   test('should return undefined when not have variable', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.LOCAL)
+
     jest.spyOn(dotenv, 'config').mockReturnValueOnce({
       parsed: {
         DB_USERNAME: 'Any Name',
@@ -46,4 +87,66 @@ describe('EnvironmentConfiguration', () => {
       password: 'Any Password'
     })
   })
+
+  test('should return undefined when not have variable and environment is test', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.TEST)
+
+    jest.spyOn(dotenv, 'config').mockReturnValueOnce({
+      parsed: {
+        TEST_DB_USERNAME: 'Any Name',
+        TEST_DB_PASSWORD: 'Any Password',
+        TEST_DB_HOST: undefined
+      }
+    })
+
+    const response = EnvironmentConfiguration.database()
+    expect(response).toMatchObject({
+      host: undefined,
+      port: 3306,
+      username: 'Any Name',
+      password: 'Any Password'
+    })
+  })
+
+  test('should return port when test', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.TEST)
+
+    jest.spyOn(dotenv, 'config').mockReturnValueOnce({
+      parsed: {
+        TEST_APP_PORT: "446"
+      }
+    })
+
+    const response = EnvironmentConfiguration.appPort()
+    expect(response).toBe(446)
+  })
+
+  test('should return port when local', () => {
+    jest.spyOn(EnvironmentConfiguration, 'environment')
+      .mockReturnValueOnce(Environment.LOCAL)
+
+    jest.spyOn(dotenv, 'config').mockReturnValueOnce({
+      parsed: {
+        APP_PORT: "458"
+      }
+    })
+
+    const response = EnvironmentConfiguration.appPort()
+    expect(response).toBe(458)
+  })
+
+  test('should return TEST when process return test', () => {
+    process.env.NODE_ENV = "test"
+    const response = EnvironmentConfiguration.environment()
+    expect(response).toEqual(Environment.TEST)
+  })
+
+  test('should return TEST when process return local', () => {
+    process.env.NODE_ENV = "local"
+    const response = EnvironmentConfiguration.environment()
+    expect(response).toEqual(Environment.LOCAL)
+  })
+
 })
